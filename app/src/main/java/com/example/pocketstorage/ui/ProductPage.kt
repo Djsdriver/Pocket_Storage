f706c790616743665724e35879eba2513f2de5a3
Djsdriver <alexei.gra4ev@gmail.com>
2023-11-28 15:26:56 +0300

    Task_branch_7_add_product: Добавлена работа с toolbar

--- app/src/main/java/com/example/pocketstorage/ui/ProductPage.kt
+++ app/src/main/java/com/example/pocketstorage/ui/ProductPage.kt
@@ -1,9 +1,9 @@
 package com.example.pocketstorage.ui
 
-import android.widget.ImageButton
+import android.telecom.Call.Details
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -13,44 +13,44 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentWidth
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.PagerState
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.LeadingIconTab
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.ScrollableTabRow
 import androidx.compose.material3.Tab
 import androidx.compose.material3.TabRow
+import androidx.compose.material3.TabRowDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ImageBitmap
-import androidx.compose.ui.graphics.PaintingStyle.Companion.Stroke
 import androidx.compose.ui.graphics.PathEffect
 import androidx.compose.ui.graphics.drawscope.Stroke
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.core.content.ContextCompat
 import com.example.pocketstorage.R
 import com.example.pocketstorage.ui.theme.PocketStorageTheme
+import kotlinx.coroutines.launch
 
 @Composable
 fun ProductPage() {
@@ -150,6 +150,7 @@ fun DashedBorderWithImage() {
     }
 }
 
+@OptIn(ExperimentalFoundationApi::class)
 @Preview(showBackground = true)
 @Composable
 fun TabScreen() {
@@ -157,7 +158,20 @@ fun TabScreen() {
 
     val tabs = listOf("Details", "Location", "QR")
 
-    Column() {
+    val pagerState = rememberPagerState(initialPage = tabIndex)
+    
+    LaunchedEffect(tabIndex){
+        pagerState.animateScrollToPage(tabIndex)
+    }
+
+    LaunchedEffect(pagerState.currentPage,pagerState.isScrollInProgress){
+        if(!pagerState.isScrollInProgress){
+            tabIndex=pagerState.currentPage
+        }
+
+    }
+
+    Column {
         ScrollableTabRow(
             edgePadding = (0).dp,
             selectedTabIndex = tabIndex,
@@ -168,39 +182,82 @@ fun TabScreen() {
             },
             divider = {
 
-            },
-
+            }
         ) {
             tabs.forEachIndexed { index, title ->
-                Tab(text = { Text(title) },
-                    selected = tabIndex == index,
-                    onClick = { tabIndex = index },
+                Tab(
+                    text = { Text(title) },
+                    selected = index==tabIndex,
+                    onClick = {
+                        tabIndex = index
+                    },
                     selectedContentColor = colorResource(id = R.color.blue),
                     unselectedContentColor = colorResource(id = R.color.gray_tab)
                 )
             }
         }
-        when (tabIndex) {
-            0 -> HomeScreen()
-            1 -> AboutScreen()
-            2 -> SettingsScreen()
+
+        HorizontalPager(pageCount = tabs.size, state = pagerState) { page ->
+            when (page) {
+                0 -> TabItem.DetailsScreen.screen.invoke()
+                1 -> TabItem.LocationsScreen.screen.invoke()
+                2 -> TabItem.QRScreen.screen.invoke()
+            }
         }
     }
 }
 
+
+
+
+typealias ComposableFun = @Composable () -> Unit
+
+sealed class TabItem(var title: String, var screen: ComposableFun) {
+    object DetailsScreen : TabItem("Details", { DetailsScreen() })
+    object LocationsScreen : TabItem("Location", { LocationsScreen() })
+    object QRScreen : TabItem("QR", { QRScreen() })
+}
+
+
 @Composable
-fun SettingsScreen() {
-    Text(text = "QR")
+fun QRScreen() {
+            Box(modifier = Modifier
+                .fillMaxWidth()
+                .height(300.dp)
+                .background(Color.Gray),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(text = "QR")
+            }
+
+
 }
 
 @Composable
-fun AboutScreen() {
-    Text(text = "Location")
+fun LocationsScreen() {
+    Box(modifier = Modifier
+        .fillMaxWidth()
+        .height(300.dp)
+        .background(Color.Gray),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = "Location")
+    }
+
+
 
 }
 
 @Composable
-fun HomeScreen() {
-    Text(text = "Details")
+fun DetailsScreen() {
+    Box(modifier = Modifier
+        .fillMaxWidth()
+        .height(300.dp)
+        .background(Color.Gray),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = "Details")
+    }
+
 
 }
