8cba39c49277f8940a82648939fbc97dd47815ca
Djsdriver <alexei.gra4ev@gmail.com>
2023-12-01 15:18:50 +0300

    Task_branch_category_screen

---
+++ app/src/main/java/com/example/pocketstorage/ui/Category.kt
@@ -0,0 +1,340 @@
+package com.example.pocketstorage.ui
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldColors
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.pocketstorage.R
+
+@Composable
+fun Inventory() {
+    InventoryScreen()
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview(showBackground = true)
+@Composable
+fun InventoryScreen() {
+
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+
+        Row(
+            modifier = Modifier
+                .padding(top = 56.dp, start = 24.dp, bottom = 24.dp)
+                .fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+
+
+            Box(Modifier.weight(1f)) {
+                TextFieldSearchProductName(
+                    modifier = Modifier
+                        .padding(end = 24.dp)
+                        .fillMaxWidth(),
+                    label = {
+                        Text(
+                            text = "product name or id",
+                            color = colorResource(id = R.color.gray)
+                        )
+                    },
+                    leadingIcon = {
+                        Icon(
+                            imageVector = Icons.Default.Search,
+                            contentDescription = "SearchById"
+                        )
+                    },
+                    colors = TextFieldDefaults.outlinedTextFieldColors(
+                        focusedBorderColor = colorResource(id = R.color.blue),
+                        unfocusedBorderColor = colorResource(id = R.color.gray)
+                    )
+                )
+            }
+
+        }
+
+        Row(
+            modifier = Modifier
+                .padding(start = 24.dp, bottom = 16.dp),
+            horizontalArrangement = Arrangement.Start,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+
+
+            ButtonInventoryScreen(
+                modifier = Modifier.wrapContentWidth(),
+                rowContent = {
+                    Icon(
+                        imageVector = Icons.Default.Add,
+                        contentDescription = "Add category",
+                        modifier = Modifier.padding(end = 15.dp)
+                    )
+                    Text(text = "Add category", fontSize = 16.sp)
+                },
+                onClick = { /* Обработчик нажатия кнопки */ }
+            )
+
+        }
+
+        //recycler
+        val list = mutableListOf<CategoryModel>()
+        list.add(CategoryModel("Printer", R.drawable.ic_launcher_foreground, "Products: 4"))
+        list.add(CategoryModel("Monitor", R.drawable.ic_launcher_foreground, "Products: 4"))
+        list.add(CategoryModel("Printer", R.drawable.ic_launcher_foreground, "Products: 4"))
+        list.add(CategoryModel("Monitor", R.drawable.ic_launcher_foreground, "Products: 4"))
+        list.add(CategoryModel("Printer", R.drawable.ic_launcher_foreground, "Products: 4"))
+        list.add(CategoryModel("Monitor", R.drawable.ic_launcher_foreground, "Products: 4"))
+        list.add(CategoryModel("Printer", R.drawable.ic_launcher_foreground, "Products: 4"))
+        list.add(CategoryModel("Monitor", R.drawable.ic_launcher_foreground, "Products: 4"))
+
+        LazyColumn(
+            modifier = Modifier
+                .padding(start = 24.dp, end = 24.dp)
+                .background(Color.White)
+        ) {
+            items(list) { model ->
+               MyScreen()
+            }
+        }
+
+    }
+
+
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TextFieldSearchProductName(
+    modifier: Modifier,
+    label: @Composable () -> Unit,
+    leadingIcon: @Composable () -> Unit,
+    colors: TextFieldColors
+) {
+    var textIdInventory by rememberSaveable { mutableStateOf("") }
+    OutlinedTextField(
+        modifier = modifier,
+        value = textIdInventory,
+        onValueChange = { textIdInventory = it },
+        label = label,
+        leadingIcon = leadingIcon,
+        colors = colors
+    )
+}
+
+
+@Composable
+fun ButtonInventoryScreen(
+    modifier: Modifier,
+    rowContent: @Composable () -> Unit,
+    onClick: () -> Unit,
+) {
+    Button(
+        onClick = onClick,
+        shape = RoundedCornerShape(8.dp),
+        colors = ButtonDefaults.buttonColors(containerColor = colorResource(id = R.color.blue)),
+        modifier = modifier,
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            rowContent()
+        }
+    }
+}
+
+@Composable
+fun ListRow(model: CategoryModel) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .padding(2.dp)
+            .clip(RoundedCornerShape(8.dp))
+            .wrapContentHeight()
+            .fillMaxWidth()
+            .background(colorResource(id = R.color.blue_light)),
+    ) {
+        Image(
+            painter = painterResource(id = model.image),
+            contentDescription = "",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .size(100.dp)
+                .padding(5.dp)
+        )
+        Column {
+            Text(
+                text = model.name,
+                fontSize = 24.sp,
+                fontWeight = FontWeight.SemiBold,
+                color = Color.White
+            )
+            Text(
+                text = model.countProduct,
+                fontSize = 24.sp,
+                fontWeight = FontWeight.SemiBold,
+                color = Color.White
+            )
+        }
+
+    }
+}
+
+data class CategoryModel(val name: String, val image: Int, val countProduct: String)
+data class ItemsCategoryModel(val nameProduct: String)
+
+@Composable
+fun ExpandableListItem(
+    model: CategoryModel,
+    items: List<ItemsCategoryModel>,
+    onItemClick: (String) -> Unit
+) {
+    var expanded by remember { mutableStateOf(false) }
+
+    Column {
+        // Заголовок элемента списка
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .fillMaxWidth()
+                .clickable { expanded = !expanded }
+                .padding(bottom = 8.dp)
+                .clip(RoundedCornerShape(8.dp))
+                .background(colorResource(id = R.color.blue_light))
+        ) {
+            Icon(
+                painter = painterResource(id = model.image),
+                contentDescription = "image",
+                tint = Color.White
+            )
+
+            Column(
+                modifier = Modifier.weight(1f)
+            ) {
+                Text(
+                    text = model.name,
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.SemiBold,
+                    color = Color.White
+                )
+                Text(
+                    text = model.countProduct,
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.SemiBold,
+                    color = Color.White
+                )
+            }
+
+            Icon(
+                modifier = Modifier.size(24.dp),
+                painter = painterResource(id = if (expanded) R.drawable.expand_more else R.drawable.expand_more_less),
+                contentDescription = "Expand/Collapse",
+                tint = Color.White
+            )
+            Spacer(modifier = Modifier.width(10.dp))
+        }
+
+
+        val state = rememberScrollState()
+        LaunchedEffect(Unit) { state.animateScrollTo(100) }
+        // Список элементов, отображаемых при развернутом состоянии
+        if (expanded) {
+            Column(
+                modifier = Modifier
+                    .size(width = 300.dp, height = 150.dp)
+                    .padding(horizontal = 8.dp)
+                    .verticalScroll(state)
+                    ) {
+                items.forEach { item ->
+                    Text(
+                        text = item.nameProduct,
+                        fontSize = 16.sp,
+                        modifier = Modifier
+                            .clickable { onItemClick(item.nameProduct) }
+                            .padding(4.dp)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MyScreen() {
+    val selectedItems = remember { mutableStateListOf<String>() }
+    val list = mutableListOf<CategoryModel>()
+    list.add(CategoryModel("Printer", R.drawable.ic_launcher_foreground, "Products: 4"))
+
+    Column  {
+        // Список расширяемых элементов
+        list.forEach { item ->
+            ExpandableListItem(
+                model = item,
+                items = listOf(
+                    ItemsCategoryModel("Philiphs 241V8L"),
+                    ItemsCategoryModel("Philiphs 241V8L"),
+                    ItemsCategoryModel("Philiphs 241V8L"),
+                    ItemsCategoryModel("Philiphs 241V8L"),
+                    ItemsCategoryModel("Philiphs 241V8L"),
+                    ItemsCategoryModel("Philiphs 241V8L"),
+                    ItemsCategoryModel("Philiphs 241V8L"),
+                    ItemsCategoryModel("Philiphs 241V8L"),),
+                onItemClick = { subitem ->
+                    if (selectedItems.contains(subitem)) {
+                        selectedItems.remove(subitem)
+                    } else {
+                        selectedItems.add(subitem)
+                    }
+                }
+            )
+        }
+
+    }
+}
