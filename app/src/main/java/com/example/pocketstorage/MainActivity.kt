1c34c5e7f0b09c0f7126ca259612543c0fa81853
Djsdriver <alexei.gra4ev@gmail.com>
2024-02-01 15:31:34 +0300

    Task_branch_firebasefirestore: Поигрались с firebaserealtime

--- app/src/main/java/com/example/pocketstorage/MainActivity.kt
+++ app/src/main/java/com/example/pocketstorage/MainActivity.kt
@@ -10,58 +10,54 @@ import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
 import com.example.pocketstorage.graphs.RootNavigationGraph
 import com.example.pocketstorage.ui.theme.PocketStorageTheme
-import com.google.firebase.FirebaseApp
+import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.firestore.FirebaseFirestore
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val db = FirebaseFirestore.getInstance()
 
-        val user: MutableMap<String, Any> = HashMap()
-        user["firstName"] = "Alex"
-        user["lastName"] = "Dima"
 
-        val buildingsList = mutableListOf<Map<String, Any>>()
+        //val buildingsList = mutableListOf<Map<String, Any>>()
         val building1: MutableMap<String, Any> = HashMap()
         building1["id"] = "abc123"
-        building1["name"] = "Building A"
-        building1["address"] = "123 Street"
+        building1["name"] = "Школа Достоевского"
+        building1["address"] = "Краснодарская 9"
         val building2: MutableMap<String, Any> = HashMap()
-        building2["id"] = "def456"
-        building2["name"] = "Building B"
-        building2["address"] = "456 Avenue"
-        buildingsList.add(building1)
-        buildingsList.add(building2)
+        building2["id"] = "abc777"
+        building2["name"] = "Школа Достоевского"
+        building2["address"] = "Краснодонская 2д"
+        //buildingsList.add(building1)
+
 
         val categoriesList = mutableListOf<Map<String, Any>>()
         val category1: MutableMap<String, Any> = HashMap()
-        category1["id"] = "cat123"
+        category1["id"] = "category123"
         category1["buildingId"] = "abc123"
-        category1["name"] = "Category A"
+        category1["name"] = "Мониторы"
         val category2: MutableMap<String, Any> = HashMap()
-        category2["id"] = "cat456"
+        category2["id"] = "category456"
         category2["buildingId"] = "def456"
-        category2["name"] = "Category B"
+        category2["name"] = "Планшеты"
         categoriesList.add(category1)
         categoriesList.add(category2)
 
         val productsList = mutableListOf<Map<String, Any>>()
         val product1: MutableMap<String, Any> = HashMap()
-        product1["id"] = "prod123"
+        product1["id"] = "product123"
         product1["categoryId"] = "cat123"
-        product1["name"] = "Product X"
+        product1["name"] = "LG monitor"
         product1["price"] = 10.99
         val product2: MutableMap<String, Any> = HashMap()
-        product2["id"] = "prod456"
+        product2["id"] = "product456"
         product2["categoryId"] = "cat456"
         product2["name"] = "Product Y"
         product2["price"] = 19.99
         productsList.add(product1)
         productsList.add(product2)
 
-        val userDocRef = db.collection("users").document()
+        /*val userDocRef = db.collection("users").document()
         userDocRef.set(user).addOnSuccessListener {
             for (buildingData in buildingsList) {
                 val buildingDocRef = userDocRef.collection("buildings").document(buildingData["id"].toString())
@@ -93,7 +89,27 @@ class MainActivity : ComponentActivity() {
             }
         }.addOnFailureListener { e ->
             Log.d("FB", "Failed to add user: ${e.localizedMessage}")
+        }*/
+        val database = FirebaseDatabase.getInstance()
+        val dataRef = database.getReference("data")
+
+        dataRef.child("users").apply {
+            // Для user1
+            child("user1").apply {
+                child("building1").setValue(building1)
+                child("building2").setValue(building2)
+                child("building1").child("category1").setValue(category1).apply {
+                    child("building1").child("category1").child("product").setValue(product1)
+                }
+            }
+
+            // Для user2
+            child("user2").apply {
+                child("building1").setValue(building1)
+                child("building1").child("category").setValue(category1)
+            }
         }
+
 
         setContent {
             PocketStorageTheme {
