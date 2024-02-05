f76f6c9b061133767a346fd6218af8b25b4a00f2
vladalexeco <vladalexeco86@gmail.com>
2024-02-05 23:53:51 +0300

    Feat: Added Registration Screen. Added registration button to Authorization screen. All strings in Authorization Screen transferred to resource file. .idea directory added to gitignore file.

--- app/src/main/java/com/example/pocketstorage/ui/screens/Authorization.kt
+++ app/src/main/java/com/example/pocketstorage/ui/screens/Authorization.kt
@@ -14,6 +14,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -29,10 +30,12 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.TextFieldValue
@@ -41,10 +44,15 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.pocketstorage.R
 
-/*@Composable
+@Preview(showBackground = true)
+@Composable
 fun Authorization() {
-    AuthorizationScreen()
-}*/
+    AuthorizationScreen(
+        onClick = {},
+        onSignUpClick = {},
+        onForgotClick = {}
+    )
+}
 
 @Composable
 @ReadOnlyComposable
@@ -102,22 +110,27 @@ fun TextFieldAuthorizationApp(
 }
 
 @Composable
-fun ButtonLogInAuthorizationApp(onClick: () -> Unit) {
+fun ButtonLogInAuthorizationApp(
+    onClick: () -> Unit,
+    colors: ButtonColors,
+    text: String,
+    iconResource: Painter
+) {
 
     Button(
         onClick = { onClick() },
-        colors = ButtonDefaults.buttonColors(containerColor = colorResource(R.color.RetroBlue)),
+        colors = colors,
         shape = RoundedCornerShape(8.dp),
         modifier = Modifier
             .padding(bottom = 24.dp)
             .size(height = 48.dp, width = 218.dp)
     ) {
         Icon(
-            painter = painterResource(id = R.drawable.baseline_login_24),
-            contentDescription = "logIn",
+            painter = iconResource,
+            contentDescription = text,
             modifier = Modifier.padding(end = 10.dp)
         )
-        Text(text = "Log In / Sign up", color = Color.White)
+        Text(text = text, color = Color.White)
 
     }
 }
@@ -131,7 +144,7 @@ fun ButtonContinueApp(onClick: () -> Unit) {
         modifier = Modifier.size(height = 48.dp, width = 312.dp),
         border = BorderStroke(2.dp, colorResource(id = R.color.RetroBlue))
     ) {
-        Text(text = "Continue without registration", color = colorResource(R.color.RetroBlue))
+        Text(text = stringResource(id = R.string.continue_without_registration), color = colorResource(R.color.RetroBlue))
 
     }
 }
@@ -146,26 +159,50 @@ fun AuthorizationScreen(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-        TextMainApp(nameApp = "Pocket Storage")
+        TextMainApp(nameApp = stringResource(id = R.string.app_name))
 
         TextFieldAuthorizationApp(
-            textHint = "Email",
+            textHint = stringResource(id = R.string.email_or_telephone),
             color = colorResource(R.color.SpanishGrey),
             modifier = Modifier.padding(bottom = 10.dp),
             keyOption = KeyboardOptions(keyboardType = KeyboardType.Email)
-        ) { Icon(imageVector = Icons.Default.Person, contentDescription = "emailIcon") }
+        ) {
+            Icon(
+                imageVector = Icons.Default.Person,
+                contentDescription = stringResource(id = R.string.email_or_telephone)
+            )
+        }
 
         TextFieldAuthorizationApp(
-            textHint = "Password",
+            textHint = stringResource(id = R.string.password),
             color = colorResource(R.color.SpanishGrey),
             modifier = Modifier.padding(bottom = 36.dp),
             keyOption = KeyboardOptions(keyboardType = KeyboardType.Password),
             visualTransformation = PasswordVisualTransformation()
-        ) { Icon(imageVector = Icons.Default.Lock, contentDescription = "emailIcon") }
-
-        ButtonLogInAuthorizationApp {
-            //обработка нажатия с регистрацией
+        ) {
+            Icon(
+                imageVector = Icons.Default.Lock,
+                contentDescription = stringResource(id = R.string.password)
+            )
         }
+
+        ButtonLogInAuthorizationApp(
+            onClick = {
+                // обработка нажатия с аутентификацией
+            },
+            colors = ButtonDefaults.buttonColors(containerColor = colorResource(R.color.RetroBlue)),
+            text = stringResource(id = R.string.log_in),
+            iconResource = painterResource(id = R.drawable.baseline_login_24)
+        )
+
+        ButtonLogInAuthorizationApp(
+            onClick = {
+                // обработка нажатия с регистрацией
+            },
+            colors = ButtonDefaults.buttonColors(containerColor = colorResource(R.color.MildGreen)),
+            text = stringResource(id = R.string.sign_up),
+            iconResource = painterResource(id = R.drawable.account_plus)
+        )
 
         ButtonContinueApp {
             // обработка нажатия без регистрации
@@ -178,7 +215,7 @@ fun AuthorizationScreen(
             modifier = Modifier
                 .align(Alignment.End)
                 .padding(bottom = 26.dp, end = 24.dp),
-            text = "v 1.0.0",
+            text = stringResource(id = R.string.app_version),
             color = Color.Black,
             fontSize = 12.sp
         )
@@ -209,9 +246,14 @@ fun PreviewAuthorization() {
             visualTransformation = PasswordVisualTransformation()
         ) { Icon(imageVector = Icons.Default.Lock, contentDescription = "emailIcon") }
 
-        ButtonLogInAuthorizationApp {
-            //обработка нажатия с регистрацией
-        }
+        ButtonLogInAuthorizationApp(
+            onClick = {
+                // обработка нажатия с регистрацией
+            },
+            colors = ButtonDefaults.buttonColors(containerColor = colorResource(R.color.RetroBlue)),
+            text = "Log In / Sign up",
+            iconResource = painterResource(id = R.drawable.baseline_login_24)
+        )
 
         ButtonContinueApp {
             // обработка нажатия без регистрации
---
+++ app/src/main/java/com/example/pocketstorage/ui/screens/Registration.kt
@@ -0,0 +1,87 @@
+package com.example.pocketstorage.ui.screens
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.pocketstorage.R
+
+@Preview(showBackground = true)
+@Composable
+fun RegistrationScreenPreview() {
+    RegistrationScreen(
+        onSignUpClick = {}
+    )
+}
+
+@Composable
+fun RegistrationScreen(
+    onSignUpClick: () -> Unit
+) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        TextMainApp(nameApp = stringResource(id = R.string.sign_up))
+
+        TextFieldAuthorizationApp(
+            textHint = stringResource(id = R.string.email_or_telephone),
+            color = colorResource(R.color.SpanishGrey),
+            modifier = Modifier.padding(bottom = 10.dp),
+            keyOption = KeyboardOptions(keyboardType = KeyboardType.Text),
+            icon = {
+                Icon(
+                    imageVector = Icons.Default.Person,
+                    contentDescription = stringResource(id = R.string.email_or_telephone)
+                )
+            }
+        )
+
+        TextFieldAuthorizationApp(
+            textHint = stringResource(id = R.string.password),
+            color = colorResource(R.color.SpanishGrey),
+            modifier = Modifier.padding(bottom = 10.dp),
+            keyOption = KeyboardOptions(keyboardType = KeyboardType.Password),
+            icon = {
+                Icon(
+                    imageVector = Icons.Default.Lock,
+                    contentDescription = stringResource(id = R.string.password)
+                )
+            }
+        )
+
+        TextFieldAuthorizationApp(
+            textHint = stringResource(id = R.string.repeat_password),
+            color = colorResource(R.color.SpanishGrey),
+            modifier = Modifier.padding(bottom = 48.dp),
+            keyOption = KeyboardOptions(keyboardType = KeyboardType.Password),
+            icon = {
+                Icon(
+                    imageVector = Icons.Default.Lock,
+                    contentDescription = stringResource(id = R.string.repeat_password)
+                )
+            }
+        )
+
+        ButtonLogInAuthorizationApp(
+            onClick = {},
+            colors = ButtonDefaults.buttonColors(containerColor = colorResource(R.color.MildGreen)),
+            text = stringResource(id = R.string.sign_up),
+            iconResource = painterResource(id = R.drawable.account_plus)
+        )
+    }
+}
